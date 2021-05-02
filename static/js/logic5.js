d93731189dc30da74118b6ab578258387fc918bb
Keri <khennenf@yahoo.com>
2021-05-01 22:07:42 -0400

    updates

--- static/js/logic5.js
+++ static/js/logic5.js
@@ -1,13 +1,14 @@
+//Data URL
 var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_month.geojson"
 
+//Declare Map
 var myMap = L.map("map", {
-    center: [37.09, -95.71],
+    center: [15.183002, -23.703451],
     zoom: 3,
   });
 
-
-
-var streetmap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+//Basemap Layer
+var basemap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
     attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
     tileSize: 512,
     maxZoom: 18,
@@ -16,35 +17,28 @@ var streetmap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}
     accessToken: API_KEY
   }).addTo(myMap)
 
-
+//Get Data
 d3.json(queryUrl, function(data){
   L.geoJson(data);
   console.log(data)
 
-  var test = data.features[1].geometry.coordinates[0]
-  console.log(test)
-
- console.log(data.features[0].geometry.coordinates[0])
-
-
-markers = []
-
-
+//Determine marker radius size based on mag
 function markerSize(mag) {
     return mag*1.5;
   }
 
-
+//Determine marker color based on depth
 function getColor(d) {
-    return d > 91 ? '#feebe2' :
-           d > 90  ? '#fcc5c0' :
-           d > 70  ? '#fa9fb5' :
-           d > 50  ? '#f768a1' :
-           d > 30   ? '#c51b8a' :
-           d > 10   ? '#7a0177' :
-                      '#FFEDA0';
+    return d > 91 ? '#8c2d04' :
+           d > 90  ? '#cc4c02' :
+           d > 70  ? '#ec7014' :
+           d > 50  ? '#fe9929' :
+           d > 30   ? '#fec44f' :
+           d > 10   ? '#fee391' :
+                      '#ffffd4';
   }
 
+  //
   for (var i = 0; i < data.features.length; i++) {
     var lng = data.features[i].geometry.coordinates[0]
     var customPopup = "<h2>Location:<br>" + data.features[i].properties.place + "</h2> <hr> <h3>Magnitude: " + data.features[i].properties.mag + "</h3>"
@@ -56,19 +50,34 @@ function getColor(d) {
         }
     var lat = data.features[i].geometry.coordinates[1]
         L.circleMarker([lat, lng], {
-        stroke: false,
-        fillOpacity: 1,
-        color: getColor(data.features[i].geometry.coordinates[2]),
-        radius: markerSize(data.features[i].properties.mag)
+        fillColor: getColor(data.features[i].geometry.coordinates[2]),
+        color: "black",
+        radius: markerSize(data.features[i].properties.mag),
+        stroke: true,
+        weight: .5,
+        fillOpacity: .8,
     }).bindPopup(customPopup, customOptions).addTo(myMap)
   }
 
-    var legend = L.control({position: 'bottomright'});
+  var legend = L.control({position: 'bottomright'});
 
-    legend.onAdd - function(map)
+  legend.onAdd = function(map) {
 
-   
+    var div = L.DomUtil.create('div', 'info legend'),
+      arr = [-10,10,30,50,70,90],
+      labels = ['<strong> Earthquake Depth (km) </strong> <hr>']
 
-}); 
+      for (var i = 0; i <arr.length; i++) {
+        
+        labels.push(
+          '<i style="background:' + getColor(arr[i] + 1) + '"></i> ' +
+          arr[i] + (arr[i + 1] ? '&ndash;' + arr[i + 1] + '<br>' : '+'));
+      }
+      div.innerHTML = labels.join('<br>');
+      return div;
+  };
+
+  legend.addTo(myMap)
+})
 
 
